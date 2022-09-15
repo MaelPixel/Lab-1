64bf7011b739630ee38c83a2828a9ebd3ebbac7b
YaNaRion <druet.goldorak@gmail.com>
2022-09-15 08:44:52 -0400

    Copie

---
+++ 2022A_TP1-master/exercice1.py
@@ -0,0 +1,23 @@
+def div3():
+    for c in int(n):
+        c+=c
+    if c%3==0:
+        print("fizz")
+        return
+    else
+    c=n
+
+
+
+
+def fizzBuzz(n):
+    div3(n)
+    # TODO imprimer la chaine de caractère appropriée avec la fonction print().
+    #  Assigner ensuite la valeur à la variable resultat et retourner celle-ci avec le mot-clé return.
+
+    resultat = n
+    return resultat
+
+if __name__ == '__main__':
+    n = int(input("indiquez le nombre: "))
+    fizzBuzz(n)
