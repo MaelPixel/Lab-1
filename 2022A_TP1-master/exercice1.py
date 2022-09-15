d0fad65f2f21887756a2bf592c84cb53153bdcdd
YaNaRion <druet.goldorak@gmail.com>
2022-09-15 09:09:16 -0400

    exercice 1 finit ou presque

--- 2022A_TP1-master/exercice1.py
+++ 2022A_TP1-master/exercice1.py
@@ -1,17 +1,28 @@
-def div3():
-    for c in int(n):
-        c+=c
-    if c%3==0:
+def div3(n):
+    if float(n)%3==0 and float(n)%5!=0:
         print("fizz")
         return
-    else
-    c=n
-
-
-
+    else:
+        return
+def div5(n):
+    if float(n)%5==0 and float(n)%3!=0:
+        print("buzz")
+        return
+    else:
+        return
+def div3_5(n):
+    if float(n)%5==0 and float(n)%3==0:
+        print("fizzbuzz")
+def ndiv3_5(n):
+    if float(n)%5!=0 and float(n)%3!=0:
+        print(n)
 
 def fizzBuzz(n):
     div3(n)
+    div5(n)
+    div3_5(n)
+    ndiv3_5(n)
+
     # TODO imprimer la chaine de caractère appropriée avec la fonction print().
     #  Assigner ensuite la valeur à la variable resultat et retourner celle-ci avec le mot-clé return.
 
